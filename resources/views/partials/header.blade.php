ee4a5f92ed56643f47617b610fe4fad3911c1f49
francs2812 <emiliofrangella2812@gmail.com>
2021-07-13 16:22:15 +0200

    header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,128 @@
+
+    <header>
+        <style>
+            nav,
+            #bar_top {
+                text-transform: uppercase
+            }
+
+            nav {
+                width: 100%;
+                height: 8vh;
+                display: flex;
+                align-content: center;
+                align-items: center;
+                justify-content: space-around;;
+                text-transform: uppercase;
+                font-size: 10px;
+            }
+
+            img {
+                    height: 85%;
+                }
+
+            ul {
+                display: flex;
+                align-items: center;
+            }
+            li {
+                padding: 10px;
+                list-style: none;
+                }
+            li:hover {
+                        cursor: pointer;
+                    }
+            li > a {
+                    text-decoration: none;
+                    color: black;
+            }
+        
+            nav > a {
+                    height: 80%;
+            }
+            
+            #bar_top {
+                padding: 5px 10%;
+                background-color: blue;
+                display: flex;
+                flex-direction: row;
+                justify-content: flex-end;
+            }
+            #bar_top > a {
+                    text-decoration: none;
+                    margin: 0 10px;
+                    color: white;
+            }
+        </style>
+        <div id="bar_top">
+            <a href="">dc power &#8482; visa &#174;</a>
+            <a href="">additional {{ $triangolo }} sites &#9660;</a>
+        </div>
+        <nav>
+            <a href="#">
+                <img src="/images/dc-logo.png" alt="logo">
+            </a>
+            <ul>
+                <li>
+                    <a href="">
+                    characters
+                    </a>
+                </li>
+                
+                <li>
+                    <a href="">
+                    comics
+                    </a>
+                </li>
+
+                <li>
+                    <a href="">
+                    movies
+                    </a>
+                </li>
+
+                <li>
+                    <a href="">
+                    tv
+                    </a>
+                </li>
+
+                <li>
+                    <a href="">
+                    games
+                    </a>
+                </li>
+
+                <li>
+                    <a href="">
+                    collectibles
+                    </a>
+                </li>
+
+                <li>
+                    <a href="">
+                    videos
+                    </a>
+                </li>
+
+                <li>
+                    <a href="">
+                    fans
+                    </a>
+                </li>
+
+                <li>
+                    <a href="">
+                    news
+                    </a>
+                </li>
+
+                <li>
+                    <a href="">
+                    shop
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+    </header>
